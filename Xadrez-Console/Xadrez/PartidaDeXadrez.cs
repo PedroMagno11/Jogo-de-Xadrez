b0d45d0d8c66654daac50f5ee5b17cf0ac49a760
Pedro Magno <pedromagnopro@gmail.com>
2022-11-25 00:22:32 -0300

    Jogada especial promocao

--- Xadrez-Console/Xadrez/PartidaDeXadrez.cs
+++ Xadrez-Console/Xadrez/PartidaDeXadrez.cs
@@ -145,6 +145,21 @@ namespace xadrez
                 throw new TabuleiroException("Você não pode se colocar em xeque!");
             }
 
+            Peca p = tab.peca(destino);
+
+            //#jogadaespecial Promocao
+            if(p is Peao)
+            {
+                if((p.cor == Cor.Branca && destino.Linha == 0) || (p.cor == Cor.Preta && destino.Linha == 7))
+                {
+                    p = tab.RetirarPeca(destino);
+                    pecas.Remove(p);
+                    Peca dama = new Dama(tab, p.cor);
+                    tab.ColocarPeca(dama, destino);
+                    pecas.Add(dama);
+                }
+            }
+
             if (EstaEmXeque(Adversaria(jogadorAtual)))
             {
                 xeque = true;
@@ -164,7 +179,6 @@ namespace xadrez
                 MudaJogador();
             }
 
-            Peca p = tab.peca(destino);
             // #jogadaespecial En Passant
             if(p is Peao && (destino.Linha == origem.Linha - 2)|| destino.Linha == origem.Linha + 2)
             {
